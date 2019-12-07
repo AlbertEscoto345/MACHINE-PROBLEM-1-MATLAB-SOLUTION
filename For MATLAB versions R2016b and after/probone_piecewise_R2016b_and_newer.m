e399dcc9488a9ab9fb01faf613880a180e656c77
AlbertEscoto345 <47411299+AlbertEscoto345@users.noreply.github.com>
2019-12-07 16:24:00 +0800

    Add files via upload

--- For MATLAB versions R2016b and after/probone_piecewise_R2016b_and_newer.m
+++ For MATLAB versions R2016b and after/probone_piecewise_R2016b_and_newer.m
@@ -11,28 +11,24 @@ stem(n,y,'rd','linewidth',1.5,'markerfacecolor','r')
 xlabel 'n'
 ylabel 'f(n)'
 title 'Discrete Sequence Data Plot from n = 0 to n = 99  of the Given Piecewise Function'
-
+  
 function  [y] = probone_piecewise(n)
-if (n<9)
+if (n<=9)
         y = n^2-7;
 elseif (n>=10)
         n = n-10;
-        if n<9
+        if n<=9
                 y = n^2-7;
-        elseif n==9
-                y = NaN;
         else
                 while n>=10
                 n = n-10;
                 end
-                if n<9
+                if n<=9
                                 y = n^2-7;
                 else
                                 y = NaN;
                 end
         end
-else
-        y = NaN;
 end
 
 end
